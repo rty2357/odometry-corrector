3c882195f260cc4aff512c2226d758904d714504
tyamada <tyamada@roboken.esys.tsukuba.ac.jp>
2013-06-11 16:37:12 +0900

    modify for gndlib change

--- src/odometry-corrector-conf.hpp
+++ src/odometry-corrector-conf.hpp
@@ -14,8 +14,7 @@
 #include "ssm-particles.hpp"
 
 
-#include "gnd-observation-probability.hpp"
-#include "gnd-configuration.hpp"
+#include "gnd-config-file.hpp"
 #include "gnd-lib-error.h"
 
 
@@ -35,8 +34,8 @@ namespace OdometryCorrector {
 namespace OdometryCorrector {
 
 	int proc_conf_initialize(proc_configuration *c);
-	int proc_conf_get(gnd::Conf::Configuration *src, proc_configuration* dest);
-	int proc_conf_set(gnd::Conf::Configuration *dest, proc_configuration* src);
+	int proc_conf_get(gnd::conf::configuration *src, proc_configuration* dest);
+	int proc_conf_set(gnd::conf::configuration *dest, proc_configuration* src);
 	int proc_conf_read(const char* f, proc_configuration* dest);
 	int proc_conf_write(const char* f, proc_configuration* src);
 
@@ -51,7 +50,7 @@ namespace OdometryCorrector {
 	static const char ssm_name[] = SSM_CORRECTED_ODOMETRY_NAME;
 
 	// map file path
-	static const gnd::Conf::parameter_array<char, 512> ConfIni_Map = {
+	static const gnd::conf::parameter_array<char, 512> ConfIni_Map = {
 			"map",
 			"",		// file path
 			"map file path"
@@ -59,35 +58,35 @@ namespace OdometryCorrector {
 
 
 	// odometry kinematics parameter: right wheel radius
-	static const gnd::Conf::parameter<double> ConfIni_WheelRadiusR = {
+	static const gnd::conf::parameter<double> ConfIni_WheelRadiusR = {
 			"wheel-radius-r",
 			0,		// right wheel radius
 			"odometry kinematics parameter: right wheel radius"
 	};
 
 	// odometry kinematics parameter: left wheel radius
-	static const gnd::Conf::parameter<double> ConfIni_WheelRadiusL = {
+	static const gnd::conf::parameter<double> ConfIni_WheelRadiusL = {
 			"wheel-radius-l",
 			0,		// left wheel radius
 			"odometry kinematics parameter: left wheel radius"
 	};
 
 	// odometry kinematics parameter: tread
-	static const gnd::Conf::parameter<double> ConfIni_Tread = {
+	static const gnd::conf::parameter<double> ConfIni_Tread = {
 			"tread",
 			0,		// tread
 			"odometry kinematics parameter: tread"
 	};
 
 	// odometry parameter: encoder resolution ( count per revolution )
-	static const gnd::Conf::parameter<double> ConfIni_CountPerRevolution = {
+	static const gnd::conf::parameter<double> ConfIni_CountPerRevolution = {
 			"count-per-rev",
 			0,		// encoder resolution ( count per revolution )
 			"odometry parameter: encoder resolution ( count per revolution )"
 	};
 
 	// odometry parameter: gear ratio
-	static const gnd::Conf::parameter<double> ConfIni_Gear = {
+	static const gnd::conf::parameter<double> ConfIni_Gear = {
 			"gear-ratio",
 			0,		// gear ratio
 			"odometry parameter: gear ratio"
@@ -96,42 +95,42 @@ namespace OdometryCorrector {
 
 
 	// name of wheel encoder shared data
-	static const gnd::Conf::parameter_array<char, 32> ConfIni_WheelEncoderName = {
+	static const gnd::conf::parameter_array<char, 32> ConfIni_WheelEncoderName = {
 			"ssm-wheel-encoder-name",
 			SNAME_PWS_MOTOR,		// shared data name
 			"name of wheel encoder shared data"
 	};
 
 	// id of wheel encoder shared data
-	static const gnd::Conf::parameter<int> ConfIni_WheelEncoderID = {
+	static const gnd::conf::parameter<int> ConfIni_WheelEncoderID = {
 			"ssm-wheel-endoder-id",
 			0,		// map file directory
 			"id of wheel encoder shared data"
 	};
 
 	// id of proofed odometry shared data
-	static const gnd::Conf::parameter_array<char, 32> ConfIni_OdometryErrorName = {
+	static const gnd::conf::parameter_array<char, 32> ConfIni_OdometryErrorName = {
 			"ssm-odometry-error-name",
 			SNAME_ODOMETRY_ERR,		// shared data name
 			"name of proofed odometry shared data"
 	};
 
 	// id of proofed odometry shared data
-	static const gnd::Conf::parameter<int> ConfIni_OdometryErrorID = {
+	static const gnd::conf::parameter<int> ConfIni_OdometryErrorID = {
 			"ssm-odometry-error-id",
 			0,		// map file directory
 			"id of proofed odometry shared data"
 	};
 
 	// name of position shared data
-	static const gnd::Conf::parameter_array<char, 32> ConfIni_PositionName = {
+	static const gnd::conf::parameter_array<char, 32> ConfIni_PositionName = {
 			"ssm-position-name",
 			SNAME_ODOMETRY,		// map file directory
 			"name of odometry error estimation shared data"
 	};
 
 	// id of position shared data
-	static const gnd::Conf::parameter<int> ConfIni_PositionID = {
+	static const gnd::conf::parameter<int> ConfIni_PositionID = {
 			"ssm-position-id",
 			0,		// map file directory
 			"name of odometry error estimation shared data"
@@ -150,18 +149,18 @@ namespace OdometryCorrector {
 		 * @brief process configuration parameter
 		 */
 		struct proc_configuration {
-			gnd::Conf::parameter_array<char, 512>	map;		///< @brief road surface map
-			gnd::Conf::parameter<double>			radius_r;	///< @brief odometry kinematics parameter : right wheel radius
-			gnd::Conf::parameter<double>			radius_l;	///< @brief odometry kinematics parameter : left wheel radius
-			gnd::Conf::parameter<double>			tread;		///< @brief odometry kinematics parameter : tread
-			gnd::Conf::parameter<double>			count_rev;	///< @brief odometry parameter : encoder resolution ( count per revolution )
-			gnd::Conf::parameter<double>			gear;		///< @brief odometry parameter : gear ratio
-			gnd::Conf::parameter_array<char, 32>	wenc_name;	///< @brief name of shared wheel encoder data
-			gnd::Conf::parameter<int>				wenc_id;	///< @brief id of shared wheel encoder data
-			gnd::Conf::parameter_array<char, 32>	odmerr_name;	///< @brief name of shared proofed odometry data
-			gnd::Conf::parameter<int>				odmerr_id;	///< @brief id of shared proofed odometry data
-			gnd::Conf::parameter_array<char, 32>	pos_name;	///< @brief name of shared position data
-			gnd::Conf::parameter<int>				pos_id;		///< @brief id of shared position data
+			gnd::conf::parameter_array<char, 512>	map;		///< @brief road surface map
+			gnd::conf::parameter<double>			radius_r;	///< @brief odometry kinematics parameter : right wheel radius
+			gnd::conf::parameter<double>			radius_l;	///< @brief odometry kinematics parameter : left wheel radius
+			gnd::conf::parameter<double>			tread;		///< @brief odometry kinematics parameter : tread
+			gnd::conf::parameter<double>			count_rev;	///< @brief odometry parameter : encoder resolution ( count per revolution )
+			gnd::conf::parameter<double>			gear;		///< @brief odometry parameter : gear ratio
+			gnd::conf::parameter_array<char, 32>	wenc_name;	///< @brief name of shared wheel encoder data
+			gnd::conf::parameter<int>				wenc_id;	///< @brief id of shared wheel encoder data
+			gnd::conf::parameter_array<char, 32>	odmerr_name;	///< @brief name of shared proofed odometry data
+			gnd::conf::parameter<int>				odmerr_id;	///< @brief id of shared proofed odometry data
+			gnd::conf::parameter_array<char, 32>	pos_name;	///< @brief name of shared position data
+			gnd::conf::parameter<int>				pos_id;		///< @brief id of shared position data
 
 			proc_configuration();
 		};
@@ -210,22 +209,22 @@ namespace OdometryCorrector {
 		 * @param [out] dest : configuration parameter
 		 */
 		inline
-		int proc_conf_get(gnd::Conf::Configuration *src, proc_configuration* dest) {
+		int proc_conf_get(gnd::conf::configuration *src, proc_configuration* dest) {
 			gnd_assert(!src, -1, "invalid null pointer");
 			gnd_assert(!dest, -1, "invalid null pointer");
 
-			gnd::Conf::get_parameter(src, &dest->map);
-			gnd::Conf::get_parameter(src, &dest->radius_r);
-			gnd::Conf::get_parameter(src, &dest->radius_l);
-			gnd::Conf::get_parameter(src, &dest->tread);
-			gnd::Conf::get_parameter(src, &dest->count_rev);
-			gnd::Conf::get_parameter(src, &dest->gear);
-			gnd::Conf::get_parameter(src, &dest->wenc_name);
-			gnd::Conf::get_parameter(src, &dest->wenc_id);
-			gnd::Conf::get_parameter(src, &dest->odmerr_name);
-			gnd::Conf::get_parameter(src, &dest->odmerr_id);
-			gnd::Conf::get_parameter(src, &dest->pos_name);
-			gnd::Conf::get_parameter(src, &dest->pos_id);
+			gnd::conf::get_parameter(src, &dest->map);
+			gnd::conf::get_parameter(src, &dest->radius_r);
+			gnd::conf::get_parameter(src, &dest->radius_l);
+			gnd::conf::get_parameter(src, &dest->tread);
+			gnd::conf::get_parameter(src, &dest->count_rev);
+			gnd::conf::get_parameter(src, &dest->gear);
+			gnd::conf::get_parameter(src, &dest->wenc_name);
+			gnd::conf::get_parameter(src, &dest->wenc_id);
+			gnd::conf::get_parameter(src, &dest->odmerr_name);
+			gnd::conf::get_parameter(src, &dest->odmerr_id);
+			gnd::conf::get_parameter(src, &dest->pos_name);
+			gnd::conf::get_parameter(src, &dest->pos_id);
 			return 0;
 		}
 
@@ -236,22 +235,22 @@ namespace OdometryCorrector {
 		 * @param [in]  src  : configuration parameter
 		 */
 		inline
-		int proc_conf_set(gnd::Conf::Configuration *dest, proc_configuration* src) {
+		int proc_conf_set(gnd::conf::configuration *dest, proc_configuration* src) {
 			gnd_assert(!src, -1, "invalid null pointer");
 			gnd_assert(!dest, -1, "invalid null pointer");
 
-			gnd::Conf::set_parameter(dest, &src->map);
-			gnd::Conf::set_parameter(dest, &src->radius_r);
-			gnd::Conf::set_parameter(dest, &src->radius_l);
-			gnd::Conf::set_parameter(dest, &src->tread);
-			gnd::Conf::set_parameter(dest, &src->gear);
-			gnd::Conf::set_parameter(dest, &src->count_rev);
-			gnd::Conf::set_parameter(dest, &src->wenc_name);
-			gnd::Conf::set_parameter(dest, &src->wenc_id);
-			gnd::Conf::set_parameter(dest, &src->odmerr_name);
-			gnd::Conf::set_parameter(dest, &src->odmerr_id);
-			gnd::Conf::set_parameter(dest, &src->pos_name);
-			gnd::Conf::set_parameter(dest, &src->pos_id);
+			gnd::conf::set_parameter(dest, &src->map);
+			gnd::conf::set_parameter(dest, &src->radius_r);
+			gnd::conf::set_parameter(dest, &src->radius_l);
+			gnd::conf::set_parameter(dest, &src->tread);
+			gnd::conf::set_parameter(dest, &src->gear);
+			gnd::conf::set_parameter(dest, &src->count_rev);
+			gnd::conf::set_parameter(dest, &src->wenc_name);
+			gnd::conf::set_parameter(dest, &src->wenc_id);
+			gnd::conf::set_parameter(dest, &src->odmerr_name);
+			gnd::conf::set_parameter(dest, &src->odmerr_id);
+			gnd::conf::set_parameter(dest, &src->pos_name);
+			gnd::conf::set_parameter(dest, &src->pos_id);
 			return 0;
 		}
 
@@ -267,7 +266,7 @@ namespace OdometryCorrector {
 
 			{ // ---> operation
 				int ret;
-				gnd::Conf::FileStream fs;
+				gnd::conf::file_stream fs;
 				// configuration file read
 				if( (ret = fs.read(f)) < 0 )	return ret;
 
@@ -287,7 +286,7 @@ namespace OdometryCorrector {
 
 			{ // ---> operation
 				int ret;
-				gnd::Conf::FileStream fs;
+				gnd::conf::file_stream fs;
 				// convert configuration declaration
 				if( (ret = proc_conf_set(&fs, src)) < 0 ) return ret;
 
